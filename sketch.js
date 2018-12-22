8077ff64adc23da6010a34003bd9ef7dd370a75a
NoNelso <jasonr4nd@gmail.com>
2018-12-21 21:09:00 -0500

    basic outline of mitosis in a peatry dish

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+var peatryDish = [];
+
 function setup() {
   createCanvas(600, 500);
+  peatryDish.push(new(cell));
+  peatryDish.push(new(cell));
 }
 
 function draw() {
+  background(220, 210, 70);
+  for (i in peatryDish) {
+    peatryDish[i].move();
+    peatryDish[i].show();
+  }
+}
 
+function mousePressed() {
+  for (i in peatryDish) {
+    if (peatryDish[i].clicked(mouseX, mouseY)) {
+      peatryDish.push(peatryDish[i].mitosis());
+      peatryDish.push(peatryDish[i].mitosis());
+      peatryDish.splice(i, 1);
+    }
+  }
 }
